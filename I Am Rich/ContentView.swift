fceaf656d3e5eab12bb5348ab1b3125406ab0028
Naidu <panaidu541@gmail.com>
2020-02-17 12:01:30 +0530

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Appala Naidu on 17/02/20.
+//  Copyright © 2020 Appala Naidu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
